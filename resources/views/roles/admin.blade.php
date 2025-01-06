0739030d9d562a85477a0503068f2c113e1012ac
farhannovaldi <farhannovaldi002@gmail.com>
2025-01-06 10:14:55 +0700

    rapiin file dashboard

---
+++ resources/views/roles/admin.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-blue-500 text-white p-4 rounded-lg">
+    <h3 class="text-xl font-bold">Welcome Admin!</h3>
+    <p>You have full access to manage users, settings, and more.</p>
+</div>
---
+++ resources/views/roles/owner_kos.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-green-500 text-white p-4 rounded-lg">
+    <h3 class="text-xl font-bold">Welcome Owner Kos!</h3>
+    <p>You can manage your kos rooms, see tenants, and update your property.</p>
+</div>
